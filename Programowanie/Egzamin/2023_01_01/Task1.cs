688aac49fc638720061db2e619eece488269d00c
Sala213 <Sala213@ezse.pl>
2025-10-30 17:28:05 +0100

    fghfhg

---
+++ Programowanie/Egzamin/2023_01_01/Task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egzamin._2023_01_01
+{
+    internal class Task1
+    {
+        public void Task()
+        {
+
+        }
+    }
+}
---
+++ Programowanie/Egzamin/Program.cs
@@ -0,0 +1,6 @@
+﻿using Egzamin._2023_01_01;
+
+Task1 task1 = new Task1();
+
+task1.Task();
+
